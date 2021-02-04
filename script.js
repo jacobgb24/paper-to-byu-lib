32aefa0fbd0af7f8f1cfd16da506f8e8f74c04d6
kimbo <kimballleavitt@gmail.com>
2021-02-04 14:12:22 -0700

    fix spacing, add springer

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     let tabUrl = new URL(tab.url);
 
     if (tabUrl.hostname.endsWith('.erl.lib.byu.edu')) {
-	alert('You\'re already on the BYU library version if this site!');
-	return;
+        alert('You\'re already on the BYU library version if this site!');
+        return;
     }
 
     // take old hostname, replace all '.' with '-', append erl.lib.byu.edu, leave rest of url as is
@@ -17,14 +17,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 // automatically redirect for some known sites
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-	 var url = new URL(details.url);
-	 url.hostname = url.hostname.replace(/\./g, '-') + ".erl.lib.byu.edu";
+         var url = new URL(details.url);
+         url.hostname = url.hostname.replace(/\./g, '-') + ".erl.lib.byu.edu";
          return {redirectUrl: url.href};
     },
     {
-	// list of known URLs to match. When you add a new one, need to also add it to permissions in manifest.json
+        // list of known URLs to match. When you add a new one, need to also add it to permissions in manifest.json
         urls: [
             "*://dl.acm.org/*",
+            "*://link.springer.com/*"
         ],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
